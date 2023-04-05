749eb51d590c4f5104e82551c7031e9ec80575fa
CaoMeiYouRen <996881204@qq.com>
2023-04-05 23:52:06 +0800

    chore: 移除不必要的注释
    fix #2

--- jest.config.ts
+++ jest.config.ts
@@ -10,11 +10,9 @@ export default {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
-    // rootDir: 'src',
     testRegex: '.test.ts$',
     transform: {
         '^.+\\.(t|j)s$': ['ts-jest', {
-            // 指定ts-jest使用的tsconfig配置
             tsconfig: 'tsconfig.json',
         }],
     },
